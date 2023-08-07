98f13ce2b1c35b050f94d3bb0053433c07095ebb
gustavo-candidoo <lg0702@hotmail.com>
2023-08-07 08:58:01 -0300

    aula 01

---
+++ Aula00/Jogo.java
@@ -0,0 +1,5 @@
+package Aula00;
+
+public class Jogo {
+
+}
---
+++ Aula01/src/Produto.java
@@ -0,0 +1,29 @@
+public class Produto{
+    //atributos ou propriedades
+    //variável de instância
+    int codigo;
+    String nome;
+    double preco;
+
+    //metódo  para imprimir os dados
+    public void imprimir(){
+        System.out.println(codigo);
+        System.out.println(nome);
+        System.out.println(preco);
+    }
+
+    public static void main(String[] args) {
+        Produto p = new Produto();
+        System.out.println(p);
+        p.codigo = 12;
+        p.nome = "arroz";
+        p.preco = 5.60;
+
+        System.out.println(p.codigo);
+        System.out.println(p.nome);
+        System.out.println(p.preco);
+
+        p.imprimir();
+
+    }
+}
