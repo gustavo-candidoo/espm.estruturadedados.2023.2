c46744e22da06e8b611ae317a903069319c92df5
Gustavo Candido <lg0702@hotmail.com>
2023-08-11 12:30:55 -0300

    s

---
+++ Estrutura de Dados/Aula02/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        Professor p = new Professor("Gustavo", 40, 110);
+
+        System.out.println(p.calcularSalario());
+    }
+}
